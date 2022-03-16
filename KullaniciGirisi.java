971d6c6cf03bda24a4a0356c7c8ca97d59e132bb
Enes Yılmaz <teknoens@gmail.com>
2022-03-16 19:15:30 +0300

    Create KullaniciGirisi.java
    Kullanıcı Girişi

---
+++ KullaniciGirisi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class KullaniciGirisi {
+    public static void main(String[] args) {
+        String userName, password;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kullanıcı Adınız : ");
+        userName = input.nextLine();
+
+        System.out.print("Şifreniz : ");
+        password = input.nextLine();
+
+        if (userName.equals("admin") && password.equals("java123")){
+            System.out.println("Giriş Yaptınız !");
+        }
+        else {
+            System.out.println("Bilgileriniz Yanlış !");
+        }
+    }
+}
