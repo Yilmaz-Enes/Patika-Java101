9357354e18458034d80b19c4c42ed00c00081a89
Enes Yılmaz <teknoens@gmail.com>
2022-04-04 03:28:11 +0300

    Create Palindrom.java
    Palindrom Sayılar

---
+++ Palindrom.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrom {
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+       boolean palindrom =  isPalindrom(242);
+       if (palindrom) {
+           System.out.println("Girilen sayı PALİNDROM' dur");
+       } else {
+           System.out.println("Girilen sayı PALİNDROM Değildir !");
+       }
+    }
+}
