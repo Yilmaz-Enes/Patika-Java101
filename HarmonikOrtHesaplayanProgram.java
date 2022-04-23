96e914397b79d9fd3453805008de1382e31dcf25
Enes Yılmaz <teknoens@gmail.com>
2022-04-23 11:58:58 +0300

    Create HarmonikOrtHesaplayanProgram.java
    Dizideki Sayıların Harmonik Ortalamasını Hesaplayan Program

---
+++ HarmonikOrtHesaplayanProgram.java
@@ -0,0 +1,12 @@
+public class HarmonikOrtHesaplayanProgram {
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+        for (int i = 0; i < list.length; i++) {
+            sum += 1.0/list[i];
+        }
+
+        double avarage = list.length / sum;
+        System.out.println(avarage);
+    }
+}
